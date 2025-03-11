32c7447074a470c1d82a579aee5041f3826eeec8
GondekNP <npg@berkeley.edu>
2025-03-11 23:08:30 +0000

    Events

---
+++ src/main/java/org/dse/JoshLang/events/EventHandler.java
@@ -0,0 +1,33 @@
+/**
+ * @license BSD-3-Clause
+ */
+package org.dse.JoshLang.core.event;
+
+import java.util.Optional;
+import org.dse.JoshLang.core.selector.CompiledSelector;
+
+/**
+ * Interface for handling events in the system.
+ */
+public interface EventHandler {
+    /**
+     * Gets the name of the attribute associated with this event handler.
+     *
+     * @return the attribute name
+     */
+    String getAttributeName();
+
+    /**
+     * Gets the name of the event this handler responds to.
+     *
+     * @return the event name
+     */
+    String getEventName();
+
+    /**
+     * Gets the conditional selector associated with this event handler.
+     *
+     * @return an Optional containing the CompiledSelector if one exists, empty otherwise
+     */
+    Optional<CompiledSelector> getConditional();
+}
