225def89c8bafa5b9cb55a906de66945465cf9fb
GondekNP <npg@berkeley.edu>
2025-03-11 23:50:01 +0000

    Config and ConfigBuilder

---
+++ src/main/java/org/dse/JoshLang/config/Config.java
@@ -0,0 +1,18 @@
+/**
+ * @license BSD-3-Clause
+ */
+package org.dse.JoshLang.config;
+
+/**
+ * Interface representing a configuration container.
+ * Provides methods to retrieve configuration values by name.
+ */
+public interface Config {
+    /**
+     * Gets a configuration value by name.
+     *
+     * @param name the name of the configuration value to retrieve
+     * @return the associated engine value
+     */
+    EngineValue getValue(String name);
+}
