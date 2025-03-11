e455fc21687795c1c317931a0a253dcd002f6b22
GondekNP <npg@berkeley.edu>
2025-03-11 02:32:53 +0000

    basic gradle build / test setup with one Addition unit test

---
+++ src/test/java/org/dse/JoshLang/EngineValueTest.java
@@ -0,0 +1,16 @@
+package org.dse.JoshLang;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EngineValueTest {
+    @Test
+    public void testAdd() {
+        EngineValue value1 = new EngineValue(3.0);
+        EngineValue value2 = new EngineValue(5.0);
+        EngineValue result = value1.add(value2);
+        EngineValue expected = new EngineValue(8.0);
+        assertEquals(expected, result, "The add method should correctly add the values");
+        assertNotNull(result, "The result should not be null");
+    }
+}
