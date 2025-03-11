c2b7ff90684290cbde8ad14775cbe74567f38037
GondekNP <npg@berkeley.edu>
2025-03-11 23:30:33 +0000

    EntityBuilder

---
+++ src/main/java/org/dse/JoshLang/entities/meta/EntityBuilder.java
@@ -0,0 +1,28 @@
+/**
+ * @license BSD-3-Clause
+ */
+package org.dse.JoshLang.entities;
+
+import org.dse.JoshLang.core.entity.meta.Entity;
+import org.dse.JoshLang.events.EventHandlerGroup;
+
+/**
+ * Builder interface for creating Entity instances.
+ * Provides methods to add event handlers and build the final entity.
+ */
+public interface EntityBuilder {
+    /**
+     * Adds event handlers to the entity being built.
+     *
+     * @param group the event handler group to add
+     * @return this builder for method chaining
+     */
+    EntityBuilder addEventHandlers(EventHandlerGroup group);
+    
+    /**
+     * Builds and returns an Entity based on the added event handlers.
+     *
+     * @return a new Entity instance
+     */
+    Entity build();
+}
