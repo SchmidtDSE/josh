82d5f21b02a457426fd05e6beb8be984895f7a63
A Samuel Pottinger <sam@gleap.org>
2025-03-12 18:07:59 +0000

    Add lint script.

---
+++ support/run_lint.sh
@@ -0,0 +1,9 @@
+if [ ! -f "checkstyle-10.21.4-all.jar" ]; then
+    wget https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.21.4/checkstyle-10.21.4-all.jar
+fi
+
+shopt -s globstar
+java -jar checkstyle-10.21.4-all.jar -c /google_checks.xml -r ./src/**/*.java
+
+overall_exit_code=$?
+exit $overall_exit_code
