1b060c07ba22e832853802351d7287a651c89cd8
A Samuel Pottinger <sam@gleap.org>
2025-03-12 21:26:34 +0000

    Resolve merge conflict #7.

--- src/main/java/org/joshsim/engine/value/Scalar.java
+++ src/main/java/org/joshsim/engine/value/Scalar.java
@@ -10,4 +10,41 @@ package org.joshsim.engine.value;
 /**
  * Engine value which only has a single discrete value.
  */
-public interface Scalar extends EngineValue {}
+public interface Scalar extends EngineValue {
+
+  /**
+   * Gets the value as a BigDecimal.
+   *
+   * @return the scalar value as a BigDecimal.
+   */
+  BigDecimal getAsDecimal();
+  
+  /**
+   * Gets the value as a boolean.
+   *
+   * @return the scalar value as a boolean.
+   */
+  boolean getAsBoolean();
+  
+  /**
+   * Gets the value as a String.
+   *
+   * @return the scalar value as a String.
+   */
+  String getAsString();
+  
+  /**
+   * Gets the value as an integer.
+   *
+   * @return the scalar value as an int.
+   */
+  int getAsInt();
+  
+  /**
+   * Gets the type of this scalar value.
+   *
+   * @return the type as a String.
+   */
+  String getType();
+  
+}
