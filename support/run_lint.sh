032c9b15f231c00ea9e2d4a1578fbdab661873e2
A Samuel Pottinger <sam@gleap.org>
2025-03-12 11:13:38 -0700

    Fix typo in run_lint.sh.

--- support/run_lint.sh
+++ support/run_lint.sh
@@ -3,7 +3,7 @@ if [ ! -f "checkstyle-10.21.4-all.jar" ]; then
 fi
 
 shopt -s globstar
-java -jar checkstyle-10.21.4-all.jar -c /google_checks.xml -r ./src/**/*.java
+java -jar checkstyle-10.21.4-all.jar -c /google_checks.xml ./src/**/*.java
 
 overall_exit_code=$?
 exit $overall_exit_code
