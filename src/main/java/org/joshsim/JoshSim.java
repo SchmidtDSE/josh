ed7c379eae61190aae79bb576aeef6a60d5f671d
A Samuel Pottinger <sam@gleap.org>
2025-03-15 18:11:34 +0000

    Fix style issues for #10.

--- src/main/java/org/joshsim/JoshSim.java
+++ src/main/java/org/joshsim/JoshSim.java
@@ -10,6 +10,7 @@
  *
  * </div>
  */
+
 package org.joshsim;
 
 import java.io.File;
@@ -27,38 +28,34 @@ import picocli.CommandLine.Parameters;
 /**
  * Command line interface for JoshSim.
  * 
- * <p>
- * Provides several subcommands like validate and run with options to help users validate simulation 
- * files or execute them through the interface.
- * </p>
- * 
+ * <p>Provides several subcommands like validate and run with options to help users validate
+ * simulation files or execute them through the interface.</p>
+ *
  * @version 1.0
  * @since 1.0
  */
 @Command(
-  name = "joshsim",
-  mixinStandardHelpOptions = true,
-  version = "1.0",
-  description = "JoshSim command line interface",
-  subcommands = {
-    JoshSim.ValidateCommand.class
-  }
+    name = "joshsim",
+    mixinStandardHelpOptions = true,
+    version = "1.0",
+    description = "JoshSim command line interface",
+    subcommands = {
+        JoshSim.ValidateCommand.class
+    }
 )
 public class JoshSim {
 
   /**
    * Command to validate a simulation file.
    * 
-   * <p>
-   * Validate command which checks if the given josh DSL code file, yielding exit code of zero if
-   * valid and parseable and non-zero otherwise.
-   * </p>
+   * <p>Validate command which checks if the given josh DSL code file, yielding exit code of zero
+   * if valid and parseable and non-zero otherwise.</p>
    */
   @Command(
-    name = "validate",
-    description = "Validate a simulation file"
+      name = "validate",
+      description = "Validate a simulation file"
   )
-  private static class ValidateCommand implements Callable<Integer> {
+  static class ValidateCommand implements Callable<Integer> {
 
     @Parameters(index = "0", description = "Path to file to validate")
     private File file;
@@ -66,11 +63,9 @@ public class JoshSim {
     /**
      * Validates the simulation file specified.
      *
-     * <p>
-     * This method ensures the file exists and reads the content a JoshLang parser, returning a
-     * non-zero exit code if the file is not found or cannot be parsed and a zero exit code
-     * otherwise.
-     * </p>
+     * <p>Ensure the file exists and read the content through a JoshLang parser, returning a non-
+     * zero exit code if the file is not found or cannot be parsed and a zero exit code
+     * otherwise.</p>
      *
      * @return an exit code indicating success (zero) or failure (non-zero).
      */
@@ -106,10 +101,8 @@ public class JoshSim {
     /**
      * Describe an error based on a RecognitionException.
      *
-     * <p>
-     * Extracts the line number and message from the given RecognitionException and return a well-
-     * formatted description of the issue that occurred during parsing.
-     * </p>
+     * <p>Extract the line number and message from the given RecognitionException and return a
+     * well- formatted description of the issue that occurred during parsing.</p>
      *
      * @param recognitionException The RecognitionException that occurred.
      * @return A formatted string describing the error, including the line number and message.
@@ -117,7 +110,7 @@ public class JoshSim {
     private String describeError(RecognitionException recognitionException) {
       int line = recognitionException.getOffendingToken().getLine();
       String description = recognitionException.getMessage();
-      String.format("Issue on line %d: %s", line, description);
+      return String.format("Issue on line %d: %s", line, description);
     }
 
     /**
@@ -127,7 +120,7 @@ public class JoshSim {
      * @return A formatted string describing the error message.
      */
     private String describeError(Exception otherException) {
-      String.format("Issue: %s", otherException.getMessage());
+      return String.format("Issue: %s", otherException.getMessage());
     }
   }
 
