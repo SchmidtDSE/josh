ef5f95741b92bbed63dea649b22b78fb5dfe3d04
A Samuel Pottinger <sam@gleap.org>
2025-03-15 22:13:40 +0000

    Add here example.

--- examples/validate.sh
+++ examples/validate.sh
@@ -52,3 +52,4 @@ assert_ok examples/slice.josh || exit 9
 assert_ok examples/simulation.josh || exit 10
 assert_ok examples/management.josh || exit 11
 assert_ok examples/external.josh || exit 12
+assert_ok examples/here.josh || exit 13
