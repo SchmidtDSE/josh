9677419fc7f4d7e8fa51417cc19ba25efe634854
A Samuel Pottinger <sam@gleap.org>
2025-03-20 20:00:25 +0000

    First implementation of Query.
    Clarifying Query and Replicate query behavior to return Patches specifically. Also, adding an implementation of Query as an object which gets utilized by the bridge in #26.

--- src/main/java/org/joshsim/engine/simulation/Query.java
+++ src/main/java/org/joshsim/engine/simulation/Query.java
@@ -6,27 +6,58 @@
 
 package org.joshsim.engine.simulation;
 
+import java.util.Optional;
 import org.joshsim.engine.geometry.Geometry;
 
 
 /**
- * A spatial and temporal query.
+ * A spatial and / or temporal query which can be used to look up Patches.
  *
  * <p>A query which refers to a specific geospatial geometry at a specific time. This provides
- * methods to retrieve geometry and time step information for querying entities.</p>
+ * methods to retrieve geometry and time step information for querying Patches.</p>
  */
-public interface Query {
+public class Query {
+
+  private final int step;
+  private final Optional<Geometry> geometry;
+
   /**
-   * Get the geometry associated with this query.
+   * Create a query which selects for all Patches in a given time step.
    *
-   * @return the geometry that defines the spatial bounds of the query
+   * @param step unique step count or time step ID for which to query.
    */
-  Geometry getGeometry();
+  public Query(int step) {
+    this.step = step;
+    this.geometry = Optional.empty();
+  }
+
+  /**
+   * Create a query which selects for both a specific time step count and geometry boundary.
+   *
+   * @param step unique step count or time step ID for which to query.
+   * @param geometry spatial bounds within which patches should be returned.
+   */
+  public Query(int step, Geometry geometry) {
+    this.step = step;
+    this.geometry = geometry;
+  }
 
   /**
    * Get the time step associated with this query.
    *
-   * @return the time step index for this query
+   * @return the time step index for this query.
    */
-  int getStep();
+  public int getStep() {
+    return step;
+  }
+  
+  /**
+   * Get the geometry associated with this query.
+   *
+   * @return the geometry that defines the spatial bounds of the query.
+   */
+  public Optional<Geometry> getGeometry() {
+    return geometry;
+  }
+
 }
