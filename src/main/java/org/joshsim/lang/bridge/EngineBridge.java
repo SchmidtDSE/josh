e66d0bdf52860058bcb673d82f12ad1f09935da8
A Samuel Pottinger <sam@gleap.org>
2025-03-20 18:34:31 +0000

    First draft of shadowing entity.

--- src/main/java/org/joshsim/lang/bridge/EngineBridge.java
+++ src/main/java/org/joshsim/lang/bridge/EngineBridge.java
@@ -5,11 +5,38 @@
  */
 
 package org.joshsim.lang.bridge;
+import org.joshsim.engine.entity.Simulation;
+import org.joshsim.engine.geometry.GeoPoint;
+import org.joshsim.engine.geometry.Geometry;
+import org.joshsim.engine.simulation.Replicate;
+import org.joshsim.engine.value.EngineValue;
 
 
 /**
  * Bridge that decouples the engine from the language interpreter.
  */
-public class EngineBridge {
+public interface EngineBridge {
 
+  Simulation getSimulation();
+  
+  void startStep();
+
+  void endStep();
+
+  void startSubstep(String phase);
+
+  void endSubstep();
+
+  ShadowingEntity getPatch(GeoPoint point);
+
+  Iterable<ShadowingEntity> getCurrentPatches();
+
+  Iterable<ShadowingEntity> getPriorPatches();
+
+  Iterable<ShadowingEntity> getPriorPatches(Geometry query);
+
+  EngineValue convert(EngineValue current, String newUnits);
+
+  Replicate end();
+  
 }
