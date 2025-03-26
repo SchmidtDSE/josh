e3206966ed7cc75fa5116ba6652bd355751a14d1
A Samuel Pottinger <sam@gleap.org>
2025-03-26 01:54:06 +0000

    Add MemberSpatialEntity.

---
+++ src/main/java/org/joshsim/engine/entity/MemberSpatialEntity.java
@@ -0,0 +1,51 @@
+/**
+ * Strcture describing entities which are in patches.
+ *
+ * @license BSD-3-Clause
+ */
+
+package org.joshsim.engine.entity;
+
+import org.joshsim.engine.geometry.Geometry;
+
+
+/**
+ * An entity with spatial properties in the system as a member of another entity.
+ *
+ * <p>A type of entity which has a specific location in geospace that can be used to find co-located
+ * entities and spatial information. This specifically refers to those which recieve geometry by
+ * being part of another entity like a Patch.</p>
+ */
+public abstract class MemberSpatialEntity implements SpatialEntity {
+  
+  private final SpatialEntity parent;
+
+  /**
+   * Create a new spatial entity with the given location.
+   *
+   * @param parent The parent entity like Patch which houses this entity.
+   */
+  public MemberSpatialEntity(SpatialEntity parent) {
+    this.parent = parent;
+  }
+
+  /**
+   * Get the geographic location of this spatial entity.
+   *
+   * @return the geographic point representing this entity's location
+   */
+  @Override
+  public Geometry getGeometry() {
+    return parent.getGeometry();
+  }
+
+  
+  /**
+   * Get the entity that houses this entity.
+   *
+   * @return The parent which houses this entity.
+   */
+  public SpatialEntity getParent() {
+    return parent;
+  }
+}
