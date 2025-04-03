0e775bec436c51a7fee2b1cafe08316bb7ccce32
GondekNP <npg@berkeley.edu>
2025-04-03 17:18:23 +0000

    ExternalPatchCacheDecorator

---
+++ src/main/java/org/joshsim/engine/external/ExternalPathCacheLayer.java
@@ -0,0 +1,34 @@
+/**
+ * Manages caching for external path resources, to avoid repeated loading.
+ *
+ * @license BSD-3-Clause
+ */
+
+package org.joshsim.engine.external;
+
+
+/**
+ * Abstract class for managing caching of external path resources.
+ */
+public abstract class ExternalPathCacheLayer extends ExternalLayerDecorator {
+  /**
+   * Constructor.
+   *
+   * @param decoratedLayer The layer to decorate
+   */
+  public ExternalPathCacheLayer(ExternalLayer decoratedLayer) {
+    super(decoratedLayer);
+  }
+
+  /**
+   * Clears any cached resources.
+   */
+  abstract void clearCache();
+
+  /**
+   * Returns the number of cached resources.
+   *
+   * @return Number of cached resources
+   */
+  abstract int getCacheSize();
+}
