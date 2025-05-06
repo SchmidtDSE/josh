44b6c8308aee8ffb47cbaba98d83b6f7749fe1b4
A Samuel Pottinger <sam@gleap.org>
2025-05-06 23:48:13 +0000

    Add tests for StreamToPrecomputedGridUtil.

--- src/test/java/org/joshsim/precompute/StreamToPrecomputedGridUtilTest.java
+++ src/test/java/org/joshsim/precompute/StreamToPrecomputedGridUtilTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
 @ExtendWith(MockitoExtension.class)
 class StreamToPrecomputedGridUtilTest {
 
