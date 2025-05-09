d923cf927324ac49b8747118ffda7b873ac9ef2e
A Samuel Pottinger <sam@gleap.org>
2025-05-09 00:00:09 +0000

    Trade off IDE

---
+++ src/main/java/org/joshsim/precompute/ExtentsTransformer.java
@@ -0,0 +1,24 @@
+/**
+ * Utility to transform an extents from Earth space to grid space.
+ *
+ * @license BSD-3-Clause
+ */
+
+package org.joshsim.precompute;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+/**
+ * Utility to transform Earth to grid space in extents.
+ */
+public class ExtentsTransformer {
+
+  /**
+   * Transform extents from Earth
+  public PatchBuilderExtents transformToGrid(PatchBuilderExtents earthExtents, double sizeMeters) {
+    
+  }
+  
+}
