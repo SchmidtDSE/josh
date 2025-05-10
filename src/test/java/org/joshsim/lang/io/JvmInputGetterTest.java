a88e8b99f869d923ae3a590f87a17f2d5ad7fea7
A Samuel Pottinger <sam@gleap.org>
2025-05-10 01:49:46 +0000

    Go green on #141

--- src/test/java/org/joshsim/lang/io/JvmInputGetterTest.java
+++ src/test/java/org/joshsim/lang/io/JvmInputGetterTest.java
@@ -7,9 +7,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 class JvmInputGetterTest {
 
