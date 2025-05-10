5df941b9e0169d4ffbd4dbbf224d587a2491dca3
Sam Pottinger <sam@gleap.org>
2025-05-10 13:43:39 -0700

    Formalize TODOs for geotiff reader.

--- src/main/java/org/joshsim/geo/external/readers/GeotiffExternalDataReader.java
+++ src/main/java/org/joshsim/geo/external/readers/GeotiffExternalDataReader.java
@@ -1,31 +1,48 @@
+/**
+ * Logic to read from geotiffs and COGs.
+ * 
+ * @license BSD-3-Clause
+ */
+
 package org.joshsim.geo.external.readers;
 
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.net.URI;
+import java.util.*;
+
 import org.apache.sis.coverage.grid.GridCoverage;
 import org.apache.sis.coverage.grid.GridGeometry;
-import org.apache.sis.referencing.CRS;
 import org.apache.sis.storage.DataStoreException;
 import org.apache.sis.storage.GridCoverageResource;
 import org.apache.sis.storage.Resource;
+import org.apache.sis.storage.StorageConnector;
 import org.apache.sis.storage.geotiff.GeoTiffStore;
+import org.apache.sis.storage.geotiff.GeoTiffStoreProvider;
 import org.joshsim.engine.value.converter.Units;
 import org.joshsim.engine.value.engine.EngineValueFactory;
 import org.joshsim.engine.value.type.EngineValue;
 import org.joshsim.geo.external.ExternalDataReader;
 import org.joshsim.geo.external.ExternalSpatialDimensions;
 import org.opengis.geometry.DirectPosition;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.util.FactoryException;
 
 
+/**
+ * Strategy which supports reading geotiffs and COGs into simulations.
+ * 
+ * <p>Strategy which supports reading geotiffs and COGs into simulations where the variable is
+ * interpreted as the band index.</p>
+ */
 public class GeotiffExternalDataReader implements ExternalDataReader {
+  private static final int STANDARD_COG_TILE_SIZE = 256;
+  
   private final EngineValueFactory valueFactory;
+  private final Map<String, double[][]> tileCache;
+  private final Units units;
+  
   private GeoTiffStore store;
   private GridCoverageResource coverage;
   private String crsCode;
@@ -35,42 +52,22 @@ public class GeotiffExternalDataReader implements ExternalDataReader {
   private BigDecimal minY;
   private BigDecimal maxY;
 
-  public GeotiffExternalDataReader(EngineValueFactory valueFactory) {
+  /**
+   * Create a new external data reader for geotiffs and COGs.
+   * 
+   * @param valueFactory The value factory to use in building values returned from this reader.
+   * @param units The units to use for the values returned from this reader.
+   */
+  public GeotiffExternalDataReader(EngineValueFactory valueFactory, Units units) {
     this.valueFactory = valueFactory;
+    this.units = units;
+    tileCache = new HashMap<>();
   }
 
   @Override
   public void open(String sourcePath) throws IOException {
     try {
-      File file = new File(sourcePath);
-      store = new GeoTiffStore(file);
-      
-      // Get the first grid coverage resource
-      for (Resource resource : store.components()) {
-        if (resource instanceof GridCoverageResource) {
-          coverage = (GridCoverageResource) resource;
-          break;
-        }
-      }
-      
-      if (coverage == null) {
-        throw new IOException("No grid coverage found in GeoTIFF file");
-      }
-
-      // Initialize geometry and bounds
-      geometry = coverage.getGridGeometry();
-      DirectPosition lower = geometry.getEnvelope().getLowerCorner();
-      DirectPosition upper = geometry.getEnvelope().getUpperCorner();
-      
-      minX = BigDecimal.valueOf(lower.getOrdinate(0)).setScale(6, RoundingMode.HALF_UP);
-      maxX = BigDecimal.valueOf(upper.getOrdinate(0)).setScale(6, RoundingMode.HALF_UP);
-      minY = BigDecimal.valueOf(lower.getOrdinate(1)).setScale(6, RoundingMode.HALF_UP);
-      maxY = BigDecimal.valueOf(upper.getOrdinate(1)).setScale(6, RoundingMode.HALF_UP);
-
-      // Get CRS
-      CoordinateReferenceSystem crs = geometry.getCoordinateReferenceSystem();
-      crsCode = CRS.getIdentifier(crs);
-      
+      openUnsafe(sourcePath);
     } catch (DataStoreException | FactoryException e) {
       throw new IOException("Failed to open GeoTIFF file: " + e.getMessage(), e);
     }
@@ -114,12 +111,14 @@ public class GeotiffExternalDataReader implements ExternalDataReader {
       List<BigDecimal> coordsY = new ArrayList<>();
       
       // Get grid size
-      int width = (int) geometry.getExtent().getSize().get(0);
-      int height = (int) geometry.getExtent().getSize().get(1);
-      
+      int width = (int) geometry.getExtent().getSize(0);
+      int height = (int) geometry.getExtent().getSize(1);
+
       // Calculate step sizes
-      BigDecimal stepX = maxX.subtract(minX).divide(BigDecimal.valueOf(width - 1), 6, RoundingMode.HALF_UP);
-      BigDecimal stepY = maxY.subtract(minY).divide(BigDecimal.valueOf(height - 1), 6, RoundingMode.HALF_UP);
+      BigDecimal stepX = maxX.subtract(minX)
+          .divide(BigDecimal.valueOf(width - 1), 6, RoundingMode.HALF_UP);
+      BigDecimal stepY = maxY.subtract(minY)
+          .divide(BigDecimal.valueOf(height - 1), 6, RoundingMode.HALF_UP);
       
       // Generate coordinate lists
       for (int i = 0; i < width; i++) {
@@ -137,9 +136,6 @@ public class GeotiffExternalDataReader implements ExternalDataReader {
   }
 
   @Override
-  private static final int TILE_SIZE = 256; // Standard COG tile size
-  private Map<String, double[][]> tileCache = new HashMap<>();
-  
   public Optional<EngineValue> readValueAt(String variableName, BigDecimal x, BigDecimal y, int timeStep) 
       throws IOException {
     try {
@@ -152,13 +148,11 @@ public class GeotiffExternalDataReader implements ExternalDataReader {
       }
 
       // Get image coordinates
-      double[] coords = new double[] {x.doubleValue(), y.doubleValue()};
-      DirectPosition position = geometry.getCoordinateReferenceSystem().getCoordinateSystem()
-        .getFactory().createDirectPosition(coords);
+      DirectPosition position = createDirectPosition(x.doubleValue(), y.doubleValue());
       
       // Calculate tile coordinates
-      int tileX = (int)(position.getOrdinate(0) / TILE_SIZE) * TILE_SIZE;
-      int tileY = (int)(position.getOrdinate(1) / TILE_SIZE) * TILE_SIZE;
+      int tileX = (int)(position.getOrdinate(0) / STANDARD_COG_TILE_SIZE) * STANDARD_COG_TILE_SIZE;
+      int tileY = (int)(position.getOrdinate(1) / STANDARD_COG_TILE_SIZE) * STANDARD_COG_TILE_SIZE;
       String tileKey = String.format("%d_%d_%d", bandIndex, tileX, tileY);
       
       // Get or load tile data
@@ -166,15 +160,14 @@ public class GeotiffExternalDataReader implements ExternalDataReader {
       if (tileData == null) {
         // Read tile from coverage
         GridCoverage data = coverage.read(null);
-        tileData = new double[TILE_SIZE][TILE_SIZE];
+        tileData = new double[STANDARD_COG_TILE_SIZE][STANDARD_COG_TILE_SIZE];
         
         // Read entire tile
-        for (int y1 = 0; y1 < TILE_SIZE; y1++) {
-          for (int x1 = 0; x1 < TILE_SIZE; x1++) {
+        for (int y1 = 0; y1 < STANDARD_COG_TILE_SIZE; y1++) {
+          for (int x1 = 0; x1 < STANDARD_COG_TILE_SIZE; x1++) {
             double[] tileValue = new double[1];
-            DirectPosition tilePos = geometry.getCoordinateReferenceSystem().getCoordinateSystem()
-              .getFactory().createDirectPosition(new double[] {tileX + x1, tileY + y1});
-            data.evaluate(tilePos, bandIndex, tileValue);
+            DirectPosition tilePos = createDirectPosition(tileX + x1, tileY + y1);
+            // TODO: fill in tileValue
             tileData[y1][x1] = tileValue[0];
           }
         }
@@ -191,12 +184,24 @@ public class GeotiffExternalDataReader implements ExternalDataReader {
       }
       
       // Create engine value with the result
-      BigDecimal value = BigDecimal.valueOf(values[0]).setScale(6, RoundingMode.HALF_UP);
-      return Optional.of(valueFactory.build(value, new Units(null)));
+      BigDecimal value = BigDecimal.valueOf(value).setScale(6, RoundingMode.HALF_UP);
+      return Optional.of(valueFactory.build(value, units));
       
     } catch (DataStoreException e) {
       throw new IOException("Failed to read value: " + e.getMessage(), e);
     }
+  }
+
+  /**
+   * Creates a DirectPosition from the specified x and y coordinates.
+   *
+   * @param x The horizontal coordinate at which the position should be created.
+   * @param y The vertical coordinate at which the position should be created.
+   * @return Newly created direct position.
+   */
+  private DirectPosition createDirectPosition(double x, double y) {
+    // TODO
+    return null;
   }
 
   @Override
@@ -221,5 +226,52 @@ public class GeotiffExternalDataReader implements ExternalDataReader {
       coverage = null;
       geometry = null;
     }
+  }
+
+  /**
+   * Open this reader using the specified source path, throwing exceptions.
+   *
+   * @param sourcePath The path to the GeoTIFF file to open. If it contains a colon, it will be
+   *     treated as a formal URI. Otherwise, it will be treated as a local file path.
+   * @throws DataStoreException Thrown if there is an issue initalizing the data store.
+   * @throws FactoryException Thrown if there is an issue initalizing the SIS factory.
+   * @throws IOException Thrown if there is an issue reading the GeoTIFF file.
+   */
+  private void openUnsafe(String sourcePath) throws DataStoreException, FactoryException,
+        IOException {
+    boolean useFullUri = sourcePath.contains(":");
+    if (useFullUri) {
+      URI fullUri = URI.create(sourcePath);
+      StorageConnector connector = new StorageConnector(fullUri);
+      GeoTiffStoreProvider provider = new GeoTiffStoreProvider();
+      store = new GeoTiffStore(null, provider, connector, true);
+    } else {
+      File file = new File(sourcePath);
+      StorageConnector connector = new StorageConnector(file);
+      GeoTiffStoreProvider provider = new GeoTiffStoreProvider();
+      store = new GeoTiffStore(null, provider, connector, true);
+    }
+
+    // Get the first grid coverage resource
+    for (Resource resource : store.components()) {
+      if (resource instanceof GridCoverageResource) {
+        coverage = (GridCoverageResource) resource;
+        break;
+      }
+    }
+
+    if (coverage == null) {
+      throw new IOException("No grid coverage found in GeoTIFF file");
+    }
+
+    // Initialize geometry and bounds
+    geometry = coverage.getGridGeometry();
+    DirectPosition lower = geometry.getEnvelope().getLowerCorner();
+    DirectPosition upper = geometry.getEnvelope().getUpperCorner();
+
+    minX = BigDecimal.valueOf(lower.getOrdinate(0)).setScale(6, RoundingMode.HALF_UP);
+    maxX = BigDecimal.valueOf(upper.getOrdinate(0)).setScale(6, RoundingMode.HALF_UP);
+    minY = BigDecimal.valueOf(lower.getOrdinate(1)).setScale(6, RoundingMode.HALF_UP);
+    maxY = BigDecimal.valueOf(upper.getOrdinate(1)).setScale(6, RoundingMode.HALF_UP);
   }
 }
