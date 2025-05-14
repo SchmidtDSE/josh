2caf001586798fd17197b4a4ebcfd8dc1714abfa
Sam Pottinger <sam@gleap.org>
2025-05-13 18:49:21 -0700

    Bump cache busters.

--- editor/js/wasm.worker.js
+++ editor/js/wasm.worker.js
@@ -1,6 +1,6 @@
-importScripts("/war/js/JoshSim.js?v=0.0.1");
-importScripts("/war/wasm-gc/JoshSim.wasm-runtime.js?v=0.0.1");
-importScripts("/js/parse.js?v=0.0.1");
+importScripts("/war/js/JoshSim.js?v=0.0.2");
+importScripts("/war/wasm-gc/JoshSim.wasm-runtime.js?v=0.0.2");
+importScripts("/js/parse.js?v=0.0.2");
 
 let wasmLayer = null;
 let postMessage = null;
