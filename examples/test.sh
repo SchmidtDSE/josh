a8d33bf3986f3578b8e4206626bc982eaaee0a73
Sam Pottinger <sam.pottinger@berkeley.edu>
2025-08-24 17:21:21 +0000

    Another CI / CD fix for #267.

--- examples/test.sh
+++ examples/test.sh
@@ -27,23 +27,23 @@ assert_ok() {
 echo "Testing CSV output..."
 rm -f /tmp/simple_josh.csv
 assert_ok examples/simulations/simple.josh TestSimpleSimulation 1 || exit 1
-[ -f "/tmp/simple_josh_1.csv" ] || exit 2
-[ -s "/tmp/simple_josh_1.csv" ] || exit 3
+[ -f "/tmp/simple_josh.csv" ] || exit 2
+[ -s "/tmp/simple_josh.csv" ] || exit 3
 
 echo "Testing CSV with Earth-space output..."
 rm -f /tmp/simple_seki_josh.csv
 assert_ok examples/simulations/simple_seki.josh TestSimpleSimulation 2 || exit 4
-[ -f "/tmp/simple_seki_josh_2.csv" ] || exit 5
-[ -s "/tmp/simple_seki_josh_2.csv" ] || exit 6
+[ -f "/tmp/simple_seki_josh.csv" ] || exit 5
+[ -s "/tmp/simple_seki_josh.csv" ] || exit 6
 
 echo "Testing netCDF output..."
-rm -f /tmp/simple_josh.nc
+rm -f /tmp/simple_josh_3.nc
 assert_ok examples/simulations/simple_netcdf.josh TestSimpleSimulation 3 || exit 7
 [ -f "/tmp/simple_josh_3.nc" ] || exit 8
 [ -s "/tmp/simple_josh_3.nc" ] || exit 9
 
 echo "Testing geotiff output..."
-rm -f /tmp/simple_josh.nc
+rm -f /tmp/simple_josh_*.tiff
 assert_ok examples/simulations/simple_geotiff.josh TestSimpleSimulation 4 || exit 10
 [ -f "/tmp/simple_josh_averageAge_0_4.tiff" ] || exit 11
 [ -s "/tmp/simple_josh_averageAge_0_4.tiff" ] || exit 12
